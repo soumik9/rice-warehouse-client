af005ba949abfd655d09134d227192352d6e23b5
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-04-29 23:07:30 +0600

    banner desing complete with responsive

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './banner.scss'
+
+const Banner = () => {
+    return (
+        <section className='banner' id='banner'>
+            <Container>
+                <div className='banner__container d-flex flex-column justify-content-center align-items-center justify-content-lg-start align-items-lg-start'>
+
+                    <h1>What are you looking for ?</h1>
+                    <h5>Search your favorite rice here to buy!</h5>
+                    <button className='btn'>Show Products</button>
+
+                </div>
+            </Container>
+        </section>
+    );
+};
+
+export default Banner;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './header.scss'
 import logo from '../../assets/images/logo.png'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
         <Navbar expand="lg">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand as={Link} to="/">
                     <img alt="" src={logo} width="30" height="33" className="d-inline-block align-top" />{' '}
                     <span>Rice Ware<i>House</i></span> 
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/">Link</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
