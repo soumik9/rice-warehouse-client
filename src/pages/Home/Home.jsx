6ba28dc412622f28ac6d8b8c9e321eff7f781ed1
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-01 03:03:31 +0600

    reviews slider added to home with responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import ChooseUs from '../../components/ChooseUs/ChooseUs';
+import Reviews from '../../components/Reviews/Reviews/Reviews';
 
 const Home = () => {
     return (
         <main>
           <Banner />
           <ChooseUs />
+          <Reviews />
         </main>
     );
 };
